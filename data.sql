633434959b1bad9e4db131f23bb5e97ed6dd0f20
Ali Bidkhori <a.bidkhori2004@gmail.com>
2024-01-17 12:09:16 +0100

    0.0.2 - Added Datenbank und test data

---
+++ data.sql
@@ -0,0 +1,8 @@
+INSERT INTO Mitarbeiter(name, admin)
+VALUES("ali", 1),("marcel", 0);
+INSERT INTO Projekte(name, Beschreibung, Mitarbeiter_ID)
+VALUES("Project1", "This is a test", 1),("Project2", "This is a test", 2);
+INSERT INTO Todos(name, Beschreibung, status, Projekt_ID)
+VALUES("Todo1", "This is a test", 0, 1),("Todo2", "This is a test", 1, 2);
+INSERT INTO Mitarbeiter_Todo(Mitarbeiter_ID, Todo_ID)
+VALUES(1,1),(2,2);
---
+++ db.sql
@@ -0,0 +1,33 @@
+Drop database if exists TodoApp;
+CREATE DATABASE TodoApp;
+
+CREATE TABLE  Mitarbeiter(
+    Mitarbeiter_ID int Primary Key IDENTITY(1,1),
+    Name varchar(100) not null,
+    Admin int
+);
+
+CREATE TABLE Projekte(
+    Projekt_ID int Primary Key IDENTITY(1,1),
+    Name varchar(100) not null,
+    Beschreibung varchar(100) not null,
+    Mitarbeiter_ID int,
+    Foreign Key (Mitarbeiter_ID) References Mitarbeiter(Mitarbeiter_ID)
+);
+
+CREATE TABLE Todos(
+    Todo_ID int Primary Key IDENTITY(1,1),
+    Name varchar(100) not null,
+    Beschreibung varchar(255) not null,
+    Status int,
+    Projekt_ID int,
+    Foreign Key (Projekt_ID) References Projekte(Projekt_ID)
+);
+
+CREATE TABLE Mitarbeiter_Todo(
+    Mitarbeiter_Todo_ID int Primary Key IDENTITY(1,1),
+    Mitarbeiter_ID int,
+    Todo_ID int,
+    Foreign Key (Mitarbeiter_ID) References Mitarbeiter(Mitarbeiter_ID),
+    Foreign Key (Todo_ID) References Todos(Todo_ID)
+);
